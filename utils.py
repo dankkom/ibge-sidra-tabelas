ba029290f3fa8fb7ce880a3c756deb394ca8c656
Daniel Komesu <danielkomesu@gmail.com>
2022-08-15 21:26:06 -0300

    🤭 Add Config class

--- utils.py
+++ utils.py
@@ -1,7 +1,22 @@
+import configparser
+import os
+from pathlib import Path
+
 import requests
 
-
 BASE_URL = "https://servicodados.ibge.gov.br/api/v3/agregados/"
+
+
+class Config:
+    def __init__(self, db_name):
+        config = configparser.ConfigParser()
+        config_dir = Path(os.getenv("CONFIG_DIR"))
+        config_filepath = config_dir / "dataengineering" / "db.ini"
+        config.read(config_filepath)
+        self.db_user = config[db_name]["user"]
+        self.db_password = config[db_name]["password"]
+        self.db_host = config[db_name]["host"]
+        self.db_port = config[db_name]["port"]
 
 
 def get_periodos(agregado):
