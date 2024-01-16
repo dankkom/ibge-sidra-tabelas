cf576ad91e27743c9244a7253696e64bf584b8b9
Daniel Komesu <danielkomesu@gmail.com>
2024-01-16 16:06:00 -0300

    Add get_filename() function

--- ibge_tabelas/utils.py
+++ ibge_tabelas/utils.py
@@ -13,3 +13,17 @@ def load_municipios():
     with resources.open_text("ibge_tabelas", "municipios.txt") as f:
         municipios = [mun.strip() for mun in f.readlines()]
     return municipios
+
+
+def get_filename(
+    sidra_tabela: str,
+    periodo: str,
+    territorial_level: str,
+    ibge_territorial_code: str,
+    variable: str = "allxp",
+):
+    name = f"t{sidra_tabela}_p{periodo}"
+    name += f"_n{territorial_level}-{ibge_territorial_code}"
+    name += f"_v-{variable}"
+    name += ".csv"
+    return name
