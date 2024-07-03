9aed5d41b61f19567c664cf7b0476feb058e2e89
Daniel Komesu <danielkomesu@gmail.com>
2024-07-02 21:12:47 -0300

    Remove .utils submodule and municipios.txt resource

--- ibge_sidra_tabelas/__init__.py
+++ ibge_sidra_tabelas/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
-from . import config, database, sidra, storage, utils
+from . import config, database, sidra, storage
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-config.setup_logging(__name__, "ibge-tabelas.log")
+config.setup_logging(__name__, "ibge-sidra-tabelas.log")
--- ibge_sidra_tabelas/utils.py
+++
@@ -1,7 +0,0 @@
-from importlib import resources
-
-
-def load_municipios():
-    with resources.open_text("ibge_tabelas", "municipios.txt") as f:
-        municipios = [mun.strip() for mun in f.readlines()]
-    return municipios
